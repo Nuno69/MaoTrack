4e6506dbcbe7bf44c285b1cb128a7acb22713119
Arkadiusz Świętnicki <nuno69a@gmail.com>
2022-07-18 21:24:48 +0200

    Initial Commit

---
+++ MaoTrack/MaoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaoTrackApp.swift
+//  MaoTrack
+//
+//  Created by Arkadiusz Świętnicki on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MaoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
